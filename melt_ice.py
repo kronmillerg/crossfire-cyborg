37afb8bcda2dfd61bb9c65c8de3fe9ddae99626f
Greg Kronmiller <kronmillerg@gmail.com>
2018-06-10 20:08:09 -0700

    Script to melt icecubes.

---
+++ melt_ice.py
@@ -0,0 +1,6 @@
+from recipe import loopCommands, Command
+
+# TODO: Detect when we're out of icecubes and stop the script.
+NUM_MELTS = 4
+loopCommands(["mark icecube"] + NUM_MELTS*["apply flint and steel"])
+
